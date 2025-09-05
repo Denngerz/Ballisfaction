90be7910e744d89b7caed10b1ebc3bce2a3f6ae8
DESKTOP-J08DHS0\denng <denngerz@gmail.com>
2025-09-05 15:36:51 +0300

    Created Project, added GAS.

---
+++ Source/BallisfactionProject/BallisfactionGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BallisfactionGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BALLISFACTIONPROJECT_API ABallisfactionGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
